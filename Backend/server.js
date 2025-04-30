da1089865011d6b83e778dd2c26aba27cb041ac8
Nishant Jhade <jhadenishant@gmail.com>
2025-04-30 14:53:51 +0530

    frontend origin added

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://zennaurawebsite.vercel.app", credentials: true }));
 app.use(bodyParser.json()); // To parse application/json
 app.use(bodyParser.urlencoded({ extended: true })); // To parse application/x-www-form-urlencoded
 
