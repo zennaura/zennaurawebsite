e39c03de0f4634c94657c591c03c27b821924188
Nishant Jhade <jhadenishant@gmail.com>
2025-05-17 15:31:14 +0530

    admin side show queries

--- Frontend/src/Admin/AdminNavbar/AdminNavbar.jsx
+++ Frontend/src/Admin/AdminNavbar/AdminNavbar.jsx
@@ -57,6 +57,16 @@ const Navbar = () => {
           </NavLink>
         </li>
 
+        <li className="sidebar-list-item">
+          <NavLink to="/queries" >
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <path d="M21.21 15.89A10 10 0 1 1 8 2.83"/>
+              <path d="M22 12A10 10 0 0 0 12 2v10z"/>
+            </svg>
+            <span>Queries</span>
+          </NavLink>
+        </li>
+
         {/* link to logout */}
         <li className="sidebar-list-item" onClick={handleLogout}>
           <NavLink to="#" >
